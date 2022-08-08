12a39396773d2b9a7c821b407035cf8de389f9a9
phamnguyentruongtin <truongtin1206@gmail.com>
2022-08-08 17:55:45 +0700

    up

---
+++ ss2loop/hienthicacloaihinh.java
@@ -0,0 +1,21 @@
+package ss2loop;
+
+import java.util.Scanner;
+
+public class hienthicacloaihinh {
+    public static void main(String[] args) {
+
+        int a = 3;
+        int b = 10;
+        for (int i = 1; i <= a; i++) {
+            for (int j = 1; j <= b; j++) {
+                System.out.printf("*");
+            }
+            System.out.println(" ");
+        }
+    }
+
+
+}
+
+
