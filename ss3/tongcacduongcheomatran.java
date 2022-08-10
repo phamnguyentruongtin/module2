7c67636251335f3c1a800710cd9100211af85050
phamnguyentruongtin <truongtin1206@gmail.com>
2022-08-10 21:15:27 +0700

    up

---
+++ ss3/tongcacduongcheomatran.java
@@ -0,0 +1,43 @@
+package ss3;
+
+import java.util.Scanner;
+
+public class tongcacduongcheomatran {
+    public static void main(String[] args) {
+        int n;
+        int sum = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Nhập vào bậc của ma trận: ");
+        n = scanner.nextInt();
+
+        int A[][] = new int[n][n];
+
+        System.out.println("Nhập các phần tử cho ma trận: ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("A[" + i + "]["+ j + "] = ");
+                A[i][j] = scanner.nextInt();
+            }
+        }
+
+        System.out.println("Ma trận vừa nhập:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(A[i][j] + "\t");
+            }
+            System.out.println("\n");
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    sum += A[i][j];
+                }
+            }
+        }
+
+        System.out.println("Tổng các phần tử nằm trên đường chéo chính = " + sum);
+    }
+}
+
